3628069062ea23910ac29c1d5850d86852e6a74c
kawamo55 <wacker@mobile.kawamo55.ddo.jp>
2019-10-17 22:54:42 +0900

    update2

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
 //
 // api_key=dxnVkKSN7rAUvcIsHlgnAD5KUMIOLc7ydh6M6Xnc
 // 
-//
+
 var express = require("express");
 var app = express();
+
+var serv = app.listen(8009,function(){
+	console.log("listening to port:"+serv.address().port);
+});
+
+app.set('view engine','ejs');
+
+app.get("/",function(req, res, next) {
+	res.render("index.ejs",{});
+});
 
